956a1e1b4aea19d686f4b19925d87198c154b5a7
aismail83 <ismailaltoumalsadick@gmail.com>
2024-02-14 11:36:36 -0500

    First release of Hello World!

--- couchimport-setup.sh
+++ couchimport-setup.sh
@@ -1,1 +1,3 @@
 sudo npm install -g couchimport
+export IAM_API_KEY="dbAWzru5JqOoFB2X9FyBVty4VhloLUWR1t6ENZ6U14kL"
+export COUCH_URL="https://a4ceb9fc-7ed5-40a7-a747-84d480d605b9-bluemix.cloudantnosqldb.appdomain.cloud"
