f31c3a403ff6c308a9bc7dc055c17e9f323e7dc2
aismail83 <ismailaltoumalsadick@gmail.com>
2024-02-14 18:41:19 -0500

    First release of Hello World!

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,50 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
+# - Any other fields you would like to include in car make model
+# - __str__ method to print a car make object
+
+class CarMake(models.Model):
+        name = models.CharField(max_length=20)
+        desc = models.CharField(max_length=200)
+
+        def __str__(self):
+                return self.name+' '+self.Description
+
+
+
+# <HINT> Create a Car Model model class CarModel(models.Model):`:
+# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Name
+# - Dealer id, used to refer a dealer created in cloudant database
+# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Year (DateField)
+# - Any other fields you would like to include in car model
+# - __str__ method to print a car make object
+
+class CarModel(models.Model):
+        carmake = models.ForeignKey('CarMake', on_delete=models.CASCADE)
+        name = models.CharField(max_length=20)
+        dealerid = models.IntegerField(default=50)
+        typel = models.CharField(max_length=9)
+        year = models.DateField()
+
+        def __str__(self):
+                return self.name+' '+self.Description+''+self.year
+
+                
+
+
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
